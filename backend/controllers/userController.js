c37bcdf415cf71627fb9da5bfdb88b976a206014
MaleeshaTHC <96755@fhss.sjp.ac.lk>
2022-06-24 12:45:59 +0530

    user registration, email login with password

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,6 +2,8 @@ const ErrorHandler = require('../utils/errorHandler')
 const catchAsyncErrors = require('../middleware/catchAsyncErrors')
 const User = require('../models/userModel')
 const sendToken = require('../utils/jwtToken')
+const sendEmail = require('../utils/sendEmail')
+const crypto = require('crypto')
 
 //Register a user
 exports.registerUser = catchAsyncErrors(async(req, res, next) => {
@@ -41,3 +43,86 @@ exports.loginUser = catchAsyncErrors( async(req, res, next) => {
 
     sendToken(user, 200, res)
 })
+
+//Logout user
+exports.logout = catchAsyncErrors( async(req, res, next) => {
+    res.cookie('token', null, {
+        expires: new Date(Date.now()),
+        httpOnly: true
+    })
+
+    res.status(200).json({
+        success: true,
+        message: 'Logged Out'
+    })
+})
+
+//Forgot password
+exports.forgotPassword = catchAsyncErrors( async(req, res, next) => {
+    const user = await User.findOne({email: req.body.email})
+
+    if(!user) {
+        return next(new ErrorHandler('User not Found', 404))
+    }
+
+    //get reset password token
+    const resetToken = user.getResetPasswordToken()
+
+    await user.save({validateBeforeSave: false})
+
+    const resetPasswordUrl = `${req.protocol}://${req.get('host')}/api/v1/password/reset/${resetToken}`
+
+    const message = `Your password reset token is: \n\n ${resetPasswordUrl} \n\nIf you have not requested this email,
+    then please ignore it`
+
+    try{
+        await sendEmail({
+            email: user.email,
+            subject: `My Tutor Password Recovery`,
+            message
+        })
+        res.status(200).json({
+            success: true,
+            message: `Email sent to ${user.email} successfully`
+        })
+
+    }catch(error) {
+        user.resetPasswordToken = undefined
+        user.resetPasswordExpire = undefined
+    }
+
+    await user.save({validateBeforeSave: false})
+
+    return next(new ErrorHandler(error.message, 500))
+}) 
+
+//Reset password
+exports.resetpassword = catchAsyncErrors(async(req, res, next) => {
+    const resetPasswordToken = crypto
+    //creating token hash
+        .createHash('sha256')
+        .update(req,params.token)
+        .digest('hex')
+
+    const user = await User.findOne({
+        resetPasswordToken,
+        resetPasswordExpire: {$gt: Date.now()}
+    })
+
+    if (!user) {
+        return next(new ErrorHandler('Reset password token is invalid or has been expired', 400))
+    }
+
+    if (req.body.password !== req.body.confirmPassword){
+        return next(new ErrorHandler('Password does not password', 400))
+    }
+
+    user.password = req.body.password
+    user.resetPasswordToken = undefined
+    user.resetPasswordExpire = undefined
+
+    await user.save()
+
+    sendToken(user, 200, res)
+})
+
--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -9,6 +9,25 @@ module.exports = (err, req, res, next) => {
         const message = `Resource not found. Invalid ${err.path}`
         err = new ErrorHandler(message, 400)
     }
+
+    //mongoose duplicate key error
+    if(err.code === 11000) {
+        const message = `Duplicate email entered`
+        err = new ErrorHandler(message, 400)
+    }
+
+    //wrong JWT error
+    if(err.name === 'jsonWebTokenError') {
+        const message = `Json web token is invalid, try again`
+        err = new ErrorHandler(message, 400)
+    }
+
+    //expired JWT error
+    if(err.name === 'TokenExpiredError') {
+        const message = `Json web token is expired, try again`
+        err = new ErrorHandler(message, 400)
+    }
+
     res.status(err.statusCode).json({
         success: false,
         message: err.message
